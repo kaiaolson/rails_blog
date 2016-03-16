fbff1056b71743ff74458ef80d4c3337768d8e2a
kaiaolson <kaiaolson@gmail.com>
2016-03-15 19:17:00 -0700

    refactor password resets controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,7 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     user.send_password_reset if user
-    redirect_to root_path, :notice => "Reset password email sent!"
+    redirect_to root_path, notice: "Reset password email sent!"
   end
 
   def edit
@@ -18,7 +18,7 @@ class PasswordResetsController < ApplicationController
     if @user.password_reset_sent_at < 3.days.ago
       redirect_to new_password_reset_path, alert: "Password reset has expired."
     elsif @user.update user_params
-      redirect_to new_session_path, :notice => "Password has been reset!"
+      redirect_to new_session_path, notice: "Password has been reset!"
     else
       render :edit
     end
